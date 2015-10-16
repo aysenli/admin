9d9bf39cb6dffd3fa70c9136049d1d1d572c4b8f
zhuayi <2179942@qq.com>
2015-10-16 15:21:51 +0800

    测试扩展包开发

--- compose-laravel.sh
+++ compose-laravel.sh
@@ -15,9 +15,11 @@ chmod -R 777 $DIRECTORY/storage $DIRECTORY/bootstrap/cache
 # 下载 build 脚本
 
 curl -sS https://raw.githubusercontent.com/zhuayi/compose-laravel/master/build.sh -o $DIRECTORY/build.sh;
-# 下载 composer.json
-curl -sS https://raw.githubusercontent.com/zhuayi/compose-laravel/master/composer.json -o $DIRECTORY/composer.json;
 
-# 执行 build 脚本, 安装 debug
 
-sh $DIRECTORY/build.sh debug
+# # 下载 composer.json
+# curl -sS https://raw.githubusercontent.com/zhuayi/compose-laravel/master/composer.json -o $DIRECTORY/composer.json;
+
+# # 执行 build 脚本, 安装 debug
+
+# sh $DIRECTORY/build.sh debug
